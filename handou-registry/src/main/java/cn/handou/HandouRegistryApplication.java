37b674be96b564b1a37812ca53a5785531626706
Figo <37700763+YourFigo@users.noreply.github.com>
2020-03-16 11:49:54 +0800

    修改 注册中心模块 启动器和配置文件

--- handou-registry/src/main/java/cn/handou/HandouRegistryApplication.java
+++ handou-registry/src/main/java/cn/handou/HandouRegistryApplication.java
@@ -1,8 +1,17 @@
 package cn.handou;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @Author Figo
  * @Date 2020/3/16 11:37
  */
+@SpringBootApplication
+@EnableEurekaServer
 public class HandouRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HandouRegistryApplication.class);
+    }
 }
