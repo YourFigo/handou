e2d6df30d44865fcf79e16d5fe8bf12e52276413
Figo <37700763+YourFigo@users.noreply.github.com>
2020-03-31 19:52:42 +0800

    修改 BrandService BrandController 增加业务逻辑

--- handou-item/handou-item-service/src/main/java/com/handou/item/controller/BrandController.java
+++ handou-item/handou-item-service/src/main/java/com/handou/item/controller/BrandController.java
@@ -1,9 +1,15 @@
 package com.handou.item.controller;
 
+import com.handou.common.pojo.PageResult;
+import com.handou.item.pojo.Brand;
 import com.handou.item.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @Author Figo
@@ -15,4 +21,28 @@ public class BrandController {
 
     @Autowired
     private BrandService brandService;
+
+    /**
+     * 根据查询条件分页查询并排序品牌信息
+     * @param key  搜索条件
+     * @param page  当前页
+     * @param rows  每页大小
+     * @param sortBy  排序字段
+     * @param desc  是否降序
+     * @return
+     */
+    @GetMapping("page")
+    public ResponseEntity<PageResult<Brand>> queryBrandByPage(
+            @RequestParam(value = "key", required = false)String key,
+            @RequestParam(value = "page", defaultValue = "1")Integer page,
+            @RequestParam(value = "rows", defaultValue = "5")Integer rows,
+            @RequestParam(value = "sortBy", required = false)String sortBy,
+            @RequestParam(value = "desc", required = false)Boolean desc
+    ){
+        PageResult<Brand> result = this.brandService.queryBrandByPage(key,page,rows,sortBy,desc);
+        if (result == null || CollectionUtils.isEmpty(result.getItems())){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
 }
--- handou-item/handou-item-service/src/main/java/com/handou/item/service/BrandService.java
+++ handou-item/handou-item-service/src/main/java/com/handou/item/service/BrandService.java
@@ -1,8 +1,16 @@
 package com.handou.item.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.handou.common.pojo.PageResult;
 import com.handou.item.mapper.BrandMapper;
+import com.handou.item.pojo.Brand;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 /**
  * @Author Figo
@@ -14,5 +22,38 @@ public class BrandService {
     @Autowired
     private BrandMapper brandMapper;
 
+    /**
+     * 根据查询条件分页查询并排序品牌信息
+     * @param key  搜索条件
+     * @param page  当前页
+     * @param rows  每页大小
+     * @param sortBy  排序字段
+     * @param desc  是否降序
+     * @return
+     */
+    public PageResult<Brand> queryBrandByPage(String key, Integer page, Integer rows, String sortBy, Boolean desc) {
 
+        // 初始化一个example对象
+        Example example = new Example(Brand.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        // 根据name模糊查询，或者根据首字母查询
+        if (StringUtils.isNotBlank(key)){
+            criteria.andLike("name","%" + key + "%").orEqualTo("letter",key);
+        }
+
+        // 添加分页条件
+        PageHelper.startPage(page,rows);
+
+        // 添加排序条件
+        if (StringUtils.isNotBlank(sortBy)){
+            example.setOrderByClause(sortBy + " " + (desc?"desc":"asc"));
+        }
+
+        List<Brand> brands = this.brandMapper.selectByExample(example);
+        // 包装成pageInfo
+        PageInfo<Brand> pageInfo = new PageInfo<>(brands);
+        // 包装成分页结果集返回
+        return new PageResult<>(pageInfo.getTotal(),pageInfo.getList());
+    }
 }
