1e86a8fad008b01176f4535f376d6f8610b8035f
Figo <37700763+YourFigo@users.noreply.github.com>
2020-04-01 22:57:00 +0800

    修改 BrandMapper 添加 新增品牌和类名中间表的Mapper方法

--- handou-item/handou-item-service/src/main/java/com/handou/item/mapper/BrandMapper.java
+++ handou-item/handou-item-service/src/main/java/com/handou/item/mapper/BrandMapper.java
@@ -1,6 +1,8 @@
 package com.handou.item.mapper;
 
 import com.handou.item.pojo.Brand;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
@@ -8,4 +10,12 @@ import tk.mybatis.mapper.common.Mapper;
  * @Date 2020/3/31 19:19
  */
 public interface BrandMapper extends Mapper<Brand> {
+
+    /**
+     * 品牌和类名中间表的新增
+     * @param cid
+     * @param id
+     */
+    @Insert("INSERT INTO tb_category_brand (category_id,brand_id) VALUES (#{cid},#{bid})")
+    void insertCategoryAndBrand(@Param("cid") Long cid, @Param("bid") Long id);
 }
