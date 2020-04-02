a7abe64baad3fed89699fadb66895424b275e4c6
Figo <37700763+YourFigo@users.noreply.github.com>
2020-04-03 03:43:31 +0800

    修改 fastDFS client 测试用例

--- handou-upload/src/test/java/com/handou/upload/test/FastDFSTest.java
+++ handou-upload/src/test/java/com/handou/upload/test/FastDFSTest.java
@@ -30,7 +30,7 @@ public class FastDFSTest {
     @Test
     public void testUpload() throws FileNotFoundException {
         // 要上传的文件
-        File file = new File("C:\\Users\\joedy\\Pictures\\xbx1.jpg");
+        File file = new File("D:\\1_Code\\java\\handou\\handou-upload\\upload\\image\\004frHf0gy728kN4ICu72&690.jpg");
         // 上传并保存图片，参数：1-上传的文件流 2-文件的大小 3-文件的后缀 4-可以不管他
         StorePath storePath = this.storageClient.uploadFile(
                 new FileInputStream(file), file.length(), "jpg", null);
@@ -42,10 +42,10 @@ public class FastDFSTest {
 
     @Test
     public void testUploadAndCreateThumb() throws FileNotFoundException {
-        File file = new File("C:\\Users\\joedy\\Pictures\\xbx1.jpg");
+        File file = new File("D:\\1_Code\\java\\handou\\handou-upload\\upload\\image\\004frHf0gy728kTfOGg7c&690.jpg");
         // 上传并且生成缩略图
         StorePath storePath = this.storageClient.uploadImageAndCrtThumbImage(
-                new FileInputStream(file), file.length(), "png", null);
+                new FileInputStream(file), file.length(), "jpg", null);
         // 带分组的路径
         System.out.println(storePath.getFullPath());
         // 不带分组的路径
