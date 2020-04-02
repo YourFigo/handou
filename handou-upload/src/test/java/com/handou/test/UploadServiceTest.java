d5fbfd16106c8b04afdd2e7d1587c9f4f1bfb148
Figo <37700763+YourFigo@users.noreply.github.com>
2020-04-02 17:25:38 +0800

    新增 handou-upload service

---
+++ handou-upload/src/test/java/com/handou/test/UploadServiceTest.java
@@ -0,0 +1,18 @@
+package com.handou.test;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @Author Figo
+ * @Date 2020/4/2 13:15
+ */
+public class UploadServiceTest {
+
+    @Test
+    public void testFile() throws IOException {
+        System.out.println(new File("").getCanonicalPath() + "\\upload\\image");
+    }
+}
