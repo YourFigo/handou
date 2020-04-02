c2d963193234ed8149605c10dcdd19e2c3d7108a
Figo <37700763+YourFigo@users.noreply.github.com>
2020-04-02 17:25:25 +0800

    新增 handou-upload service

---
+++ handou-upload/src/main/java/com/handou/upload/service/UploadService.java
@@ -0,0 +1,63 @@
+package com.handou.upload.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author Figo
+ * @Date 2020/4/2 12:38
+ */
+@Service
+public class UploadService {
+
+    private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif", "image/png",
+            "image/bmp", "image/webp", "image/x-icon", "image/vnd.microsoft.icon");
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
+
+    /**
+     * 图片上传的业务逻辑
+     * @param file
+     * @return
+     */
+    public String uploadImage(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+
+        // 校验文件类型
+        String contentType = file.getContentType();
+        if (!CONTENT_TYPES.contains(contentType)){
+            LOGGER.info("文件类型不合法：{}", originalFilename);
+            return null;
+        }
+
+        try {
+            // 检验文件内容
+            BufferedImage bufferedImage = ImageIO.read(file.getInputStream());
+            if (bufferedImage == null || bufferedImage.getHeight() == 0 || bufferedImage.getWidth() == 0){
+                LOGGER.info("文件内容不合法：{}", originalFilename);
+                return null;
+            }
+
+            // 保存到文件服务器
+            // D:\\1_Code\\java\\handou\\handou-upload\\upload\\image
+            String directory = new File("").getAbsolutePath() + "\\handou-upload\\upload\\image\\";
+            file.transferTo(new File(directory + originalFilename));
+
+            // 返回url，进行回写
+            return "http://image.handou.com/" + originalFilename;
+        } catch (IOException e) {
+            LOGGER.info("服务器内部错误：{}", originalFilename);
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
