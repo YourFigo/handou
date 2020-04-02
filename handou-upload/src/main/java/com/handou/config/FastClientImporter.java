a09a21e1c59b299b69e7d0af39b4042fd05d9aac
Figo <37700763+YourFigo@users.noreply.github.com>
2020-04-03 03:28:30 +0800

    配置 fastDFS client

---
+++ handou-upload/src/main/java/com/handou/config/FastClientImporter.java
@@ -0,0 +1,19 @@
+package com.handou.config;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+/**
+ * @Author Figo
+ * @Date 2020/4/3 3:24
+ */
+@Configuration
+@Import(FdfsClientConfig.class)
+// 解决jmx重复注册bean的问题
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class FastClientImporter {
+
+}
